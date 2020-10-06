5822918de88c234318ce30a8706125e7bf827d2c
BIT Student <classroom@bgit.rs>
2020-10-06 11:15:24 +0200

    prvi commit

---
+++ switch/tasks1_8.js
@@ -0,0 +1,114 @@
+//Task 1
+
+//primimo broj = input
+//obradimo input
+//ispisemo dan u nedelji (tekstualno) na osnovu inputa = output
+
+var input = 7
+
+switch (input) {
+    case 1: {
+        console.log("Ponedeljak");
+        break;
+    }
+    case 2: {
+        console.log("Utorak");
+        break;
+    }
+    case 3: {
+        console.log("Sreda");
+        break;
+    }
+    case 4: {
+        console.log("Cetvrtak");
+        break;
+    }
+    case 5: {
+        console.log("Petak");
+        break;
+    }
+    case 6: {
+        console.log("Subota");
+        break;
+    }
+    case 7: {
+        console.log("Nedelja");
+        break;
+    }
+}
+
+
+//Task 2
+
+var input = 8
+
+switch (input) {
+    case 1: {
+        console.log("Ponedeljak");
+        break;
+    }
+    case 2: {
+        console.log("Utorak");
+        break;
+    }
+    case 3: {
+        console.log("Sreda");
+        break;
+    }
+    case 4: {
+        console.log("Cetvrtak");
+        break;
+    }
+    case 5: {
+        console.log("Petak");
+        break;
+    }
+    case 6: {
+        console.log("Subota");
+        break;
+    }
+    case 7: {
+        console.log("Nedelja");
+        break;
+    }
+    default: {
+        console.log("Input must be a number between and 7");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
