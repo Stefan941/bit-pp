047939065488de0720d7156363cc74c9074df60a
BIT Student <classroom@bgit.rs>
2020-10-07 12:00:38 +0200

    arreys

---
+++ arrays/task1_5.js
@@ -0,0 +1,40 @@
+//Task 1 - Napisati arrya meseci u godinu
+//Putem console.log napisati JUN, OKTOBAR i JANUAR
+
+var meseci = ["januar", "februar", "mart", "april", "maj", "jun", "jul", "avgust", "septembar", "oktobar", "novembar", "decembar"];
+
+console.log(meseci[5])
+console.log(meseci[9])
+console.log(meseci[0])
+
+//Tastk 2 - Napisati dane u nedelji 
+//Putem console.loga napisati NEDELJU 
+
+var sedmica = ["ponedeljak", "utorak", "sreda", "cetvrtak", "petak", "subota", "nedelja"];
+
+console.log(sedmica[6])
+
+//Task 3 - napisati sve negativne elemente iz array-a
+
+var niz = [2, -4, 5, -2, -11];
+
+console.log(niz[1])
+console.log(niz[3])
+console.log(niz[4])
+
+//Task 4 - napisati sve elemente ciji su INDEXI deljivi sa 3 iz array-a
+
+var niz3 = [5, 15, -5, 20, 12, 18, 10, 26, 7];
+
+console.log(niz3[0])
+console.log(niz3[3])
+console.log(niz3[6])
+
+//Task 5 - sta je index broja 24 iz array-a
+
+var niz24 = [[1, 2, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+console.log(niz24[2])
+console.log(niz24[1][3])
+
+
