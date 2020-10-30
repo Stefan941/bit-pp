1d5ce8fdf57d6f90b69d8a666f51fe93b06d3f17
BIT Student <classroom@bgit.rs>
2020-10-30 11:47:02 +0100

    dodatno2

--- objects/vezbanje_6.js
+++ objects/vezbanje_6.js
@@ -2,6 +2,31 @@
 
 var input = [1, 5, 8, 2, 4, 7, 5, 6, 5, 2, 4, 2, 7];
 
-input.filter(function (element) {
+//prvi nacin
 
+var rez = [];
+for (var i = 0; i < input.length; i++) {
+    if (input[i] === 2 || input[i] === 5) {
+        rez[rez.length] = input[i]
+    }
+}
+console.log(rez)
+
+//drugi nacin
+
+var nasNiz = input.filter(function (el, i, niz) {
+    if (el === 2 || el === 5) {
+        return true
+    }
+    return false
 })
+console.log(nasNiz)
+
+//da se izbace samo 2
+
+var nasNiz = input.filter(function (el, i, niz) {
+    if (el !== 2) {
+        return true
+    }
+})
+console.log(nasNiz)
